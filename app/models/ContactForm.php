b4b529b1f664791ea67320fad90c9ab6a49ad365
melikrmain <melik.ramin@gmail.com>
2020-01-16 15:38:14 +0300

    Complated version

--- app/models/ContactForm.php
+++ app/models/ContactForm.php
@@ -1,5 +1,13 @@
 <?php
+        require 'PHPMailer/src/Exception.php';
+        require 'PHPMailer/src/PHPMailer.php';
+        require 'PHPMailer/src/SMTP.php';
+
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+
     class ContactForm {
+
         private $name;
         private $email;
         private $age;
@@ -26,17 +34,58 @@
         }
 
         public function mail() {
-            $to = "melik.ramin@gmail.com";
-            $message = 'Имя: ' . $this->name . '. Возраст: ' . $this->age . '. Сообщение: ' . $this->message;
+            //$to = "melik.ramin@gmail.com";
+            $message = 'Имя: ' . $this->name . '<br>Возраст: ' . $this->age . '<br>Сообщение: ' . $this->message;
 
-            $subject = "=?utf-8?B?".base64_encode("Сообщение с сайта")."?=";
-            $headers = "From: $this->email\r\nReply-to: $this->email\r\nContent-type: text/html; charset=utf-8\r\n";
-            $success = mail ($to, $subject, $message, $headers);
+          
+            
+            $mail = new PHPMailer(true);
 
-            if(!$success)
-                return "Сообщение было не отправлено";
-            else
-                return true;
+            define('SMTP_SERVER', 'smtp.geotek24.ru');
+            define('SMTP_PORT', 25);
+            define('SMTP_USERNAME', 'info');
+            define('SMTP_PASSWORD', '5LlqB7L5TY14');
+            define('SMTP_SENT_FROM', 'info@geotek24.ru');
+            define('SMTP_SENT_TO', $this->email);  // Please type your mail
+            define('SMTP_SENT_NAME', $this->name);  // Please type your name
+
+            $mailBody = $message;
+
+            try {
+                //Server settings
+                $mail->isSMTP();                                       // Send using SMTP
+                $mail->Host       = SMTP_SERVER;                       // Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                              // Enable SMTP authentication
+                $mail->Username   = SMTP_USERNAME;                     // SMTP username
+                $mail->Password   = SMTP_PASSWORD;                     // SMTP password
+                $mail->Port       = SMTP_PORT;                         // TCP port to connect to
+
+                //Recipients
+                $mail->setFrom(SMTP_SENT_FROM, 'Mail from site');
+                $mail->addAddress(SMTP_SENT_TO, SMTP_SENT_NAME);    
+                $mail->addReplyTo(SMTP_SENT_FROM, 'Information');
+                $mail->addCC('melik.ramin@gmail.com');
+
+                // Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'New request';
+                $mail->Body    = $mailBody;
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                return "Сообщение успешно отправлено";
+            } catch (Exception $e) {
+                return "Сообщение было не отправлено. Mailer Error: {$mail->ErrorInfo}";
+            }
+            
+            
+            
+            
+            
+            
         }
+
+
+
 
     }
